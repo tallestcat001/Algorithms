2f5b5a5f739a0a0ca7307a3f19e3282f6591cec5
tallestcat001 <shdmlgus123_@naver.com>
2024-07-12 19:46:57 +0900

    (2024.7.12) 메라토스테네스의 체

---
+++ Class2th (Sieve Of Eratosthenes)/Source.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int list[101];
+
+bool Ckeck(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+
+}
+
+void Sieve(int n)
+{
+	for (int i = 0; i <= n; i++)
+	{
+		list[i] = i;
+	}
+
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (list[i] == 0)
+		{
+			continue;
+		}
+
+		for (int j = i * 2; j <= n; j += i)
+		{
+			list[j] = 0;
+		}
+	}
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (list[i] != 0)
+		{
+			cout << list[i] << " ";
+		}
+	}
+}
+
+int main()
+{
+
+	Sieve(49);
+
+	return 0;
+}
---
+++ Program/Program.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+
+#pragma region 거품 정렬
+
+	// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘.
+
+	int list[SIZE] = { 3,11,2,7,5 };
+
+	for(int i = 0; i < SIZE - 1; i++)
+	{	
+		for (int j = 0; j < (SIZE - i) - 1; j++)
+		{
+			if (list[j] > list[j + 1])
+			{
+				swap(list[j], list[j + 1]);
+			}
+		}
+	}
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
