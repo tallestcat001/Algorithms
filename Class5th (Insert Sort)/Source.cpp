fbde15ec84638aba6e97e0cebcad5ac486052f17
tallestcat001 <shdmlgus123_@naver.com>
2024-07-15 19:46:15 +0900

    (2024.7.15) 삽입 정렬

---
+++ Class5th (Insert Sort)/Source.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+
+#pragma region 삽입 정렬
+
+	// 데이터를 하나씩 확인하면서 이미 정렬된 부분을 비교하여 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘.
+
+	int list[SIZE] = { 7,3,1,9,5 };
+
+	for (int i = 1; i < SIZE; i++)
+	{
+		int key = list[i];
+
+		for (int j = 0; j < SIZE; j++)
+		{
+			if (key < list[j])
+			{
+				int temp = list[j];
+				list[j] = key;
+				key = temp;
+			}
+		}
+
+		list[i] = key;
+
+	}
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
