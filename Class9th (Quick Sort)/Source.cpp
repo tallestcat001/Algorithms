a1be59660b1addfc22d07a9401cd7891d2835842
tallestcat001 <shdmlgus123_@naver.com>
2024-07-18 19:46:22 +0900

    (2024.7.18) 병합 정렬

---
+++ Class9th (Quick Sort)/Source.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+#define SIZE 8
+
+using namespace std;
+
+void QuickSort(int list[], int start, int end)
+{
+	if (start >= end)
+	{
+		return;
+	}
+
+	int pivot = start;
+
+	int left = start + 1;
+
+	int right = end;
+
+	while (left <= right)
+	{
+		while (left <= end && list[pivot] >= list[left])
+		{
+			left++;
+		}
+
+		while (right > start && list[pivot] <= list[right])
+		{
+			right--;
+		}
+
+		if (left > right)
+		{
+			swap(list[pivot], list[right]);
+		}
+		else
+		{
+			swap(list[left], list[right]);
+		}
+	}
+
+	QuickSort(list, start, right - 1);
+	QuickSort(list, right + 1, end);
+
+}
+
+int main()
+{
+
+#pragma region 퀵 정렬
+
+	// 기준점을 획득한 다음 해당 기준점을 기준으로
+	// 배열을 나누고 한 쪽에는 기준점보다 작은 값들이
+	// 위치하고 다른 한 쪽에는 기준점보다 큰 값들이 
+	// 위치하도록 정렬하는 알고리임.
+
+	// 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여 
+	// 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬함.
+
+	int list[SIZE] = { 5,9,6,2,1,3,4,7 };
+
+	QuickSort(list, 0, SIZE - 1);
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,7 +4,41 @@
 
 using namespace std;
 
+void MergSort(int list[],int start, int end)
+{
+	if (start < end)
+	{
+		int middle = (start + end) / 2;
+
+		MergSort(list, start, middle);
+		MergSort(list, middle + 1, end);
+	}
+}
+
+
+
 int main()
 {
+#pragma region 병합 정렬
+
+	// 하나의 리스트를 두 개의 균등한 크기로 분활하고 분활된 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를 합하여
+	// 전체가 정렬된 리스트가 되게 하는 방법.
+
+	// 1. 리스트의 길이가 0 또는 1이면 이미 정렬된 것으로 봄.
+
+	// 2. 그렇지 않은 경우 
+
+	// 2-1. 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눔.
+
+	// 2-2. 각 부분 리스트를 재귀적으로 병합 정렬을 이용하여 정렬함.
+
+	// 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합함.
+
+	int list[SIZE] = { 3,5,2,7,4,1,8,6 };
+
+	MergSort(list, 0, SIZE - 1);
+
+#pragma endregion
+
 	return 0;
 }
