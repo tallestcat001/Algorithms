8ab05eab76d63398fd5deab00fd9dd0e7e90d44e
tallestcat001 <shdmlgus123_@naver.com>
2024-07-22 19:47:09 +0900

    (2024.7.22) 탐욕법

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,40 +1,30 @@
 ﻿#include <iostream>
 
-#define SIZE 8
+#define SIZE 7
 
 using namespace std;
 
-int Fibonacci(int n)
+int Gap()
 {
-	if (n <= 0)
+	int gap = SIZE / 2;
+
+	if (gap % 2 == 0)
 	{
-		return 0;
-	}
-	else if (n == 1)
-	{
-		return 1;
-	}
-	else
-	{
-		return Fibonacci(n - 1) + Fibonacci(n - 2);
+		gap = gap + 1;
+		return gap;
 	}
 }
 
 int main()
 {
 
-#pragma region 동적 계획법
+#pragma region 쉘 정렬
 
-	// 특정 범위까지의 값을 구하기 위해서 그것과 다른 범위 까지의 값을 이용하여 효율적으로 값을 구하는 알고리즘.
+	// 먼 거리의 요소들을 먼저 정렬하여 배열을 부분적으로 정렬한 후 점진적으로 더 작은 간격을 사용하는 정렬 알고리즘.
 
-	// 메모이제이션
-	// 프로그램이 동일한 계산을 반복해야할때, 이전에 계산한 값을 메모리에 저장함으로써 
-	// 동일한 게산을 반복 수행하는 작업을 제거하여 프로그램의 실행 속도를 향상시키는 기법.
 
-	cout << Fibonacci(43) << endl;
 
 #pragma endregion
-
 
 	return 0;
 }
