1e8cf5246556077bdb491bbef5ecc17217e68a79
tallestcat001 <shdmlgus123_@naver.com>
2024-07-24 19:46:32 +0900

    (2024.7.24) 백 트래킹

---
+++ Class14th (Eucild)/Source.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <math.h>
+
+#define SIZE 11
+
+using namespace std;
+
+int Eucild(int a, int b)
+{
+	int c = 0;
+
+	while (b != 0)
+	{
+		c = a % b;
+
+		if (c == 0)
+		{
+			return b;
+		}
+
+		a = b;
+		b = c;
+	}
+}
+
+int main()
+{
+
+#pragma region 유클리트 호제법
+
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로,
+	// 두 수가 사로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘.
+
+	cout << "최대공약수는 : " << Eucild(30, 15) << " ";
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,25 +4,27 @@
 
 using namespace std;
 
+void Hanoi(int n, char from, char by, char to)
+{
+
+}
+
 int main()
 {
 
-#pragma region 최대공약수와 최소공배수
+#pragma region 백 트레킹
 
-	int a = 15;
-	int b = 30;
-	int result = 0;
+	// 해를 찾아가는 도중 지금의 경로가 해가 될 것 같지 않으면,
+	// 더이상 깊이 들어가지 않고 이전 단계로 다시 돌아가는 알고리즘.
 
-	for (int i = 2; i <= a && i <= b; i++)
-	{
-		if (a % i == 0 && b % i == 0)
-		{
-			result =+ i;
-		}
-	}
+	// 백 트래킹의 유망성 판단
+	// 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면,
+	// 그 노도의 이전 노드로 돌아가 다음 자식 노드로 이동함.
 
-	cout << "최대공약수는 : " << result << endl;
-	
+	// 해가 될 만한 가능성이 있으면 유망하다. (Promising)
+	// 유망하지 않은 노드에 가지 않는 것 (Pruning)
+
+
 
 #pragma endregion
 
