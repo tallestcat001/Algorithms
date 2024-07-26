0bbd67e8f7851e42612e11739663a1169120d23f
tallestcat001 <shdmlgus123_@naver.com>
2024-07-26 19:46:49 +0900

    (2024.7.26) 너비 우선 탐색

---
+++ Class17th (Breadth First Search)/Source.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+#define SIZE 6
+
+using namespace std;
+
+class Graph
+{
+private:
+	queue<int> queue;
+	bool visited[SIZE];
+	vector<int> graph[SIZE];
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = false;
+		}
+	}
+
+	void Insert(int vertex, int edge)
+	{
+		graph[vertex].push_back(edge);
+		graph[edge].push_back(vertex);
+	}
+
+	void Search(int start)
+	{
+		queue.push(start);
+		visited[start] = true;
+
+		while (queue.empty() == false)
+		{
+			int x = queue.front();
+
+			queue.pop();
+
+			cout << x << " ";
+
+			for (int i = 0; i < graph[x].size(); i++)
+			{
+				int next = graph[x][i];
+
+				if (visited[next] == false)
+				{
+					queue.push(next);
+					visited[next] = true;
+				}
+			}
+		}
+	}
+};
+
+int main()
+{
+
+#pragma region 넓이 우선 탐색
+
+	// 시작 정점을 방문한 후 시작 정점에 인접한 모든 정점들을 우선 방문하는 알고리즘.
+
+	// 더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 모든 정점들에 대해서 너비 우선 탐색을 적용함.
+
+	Graph graph;
+
+	graph.Insert(1, 2);
+	graph.Insert(1, 3);
+
+	graph.Insert(2, 4);
+
+	graph.Insert(3, 4);
+	graph.Insert(3, 5);
+
+	graph.Insert(4, 5);
+
+	graph.Search(1);
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,13 +1,44 @@
 ﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+#define SIZE 8
 
 using namespace std;
+
+class Graph
+{
+private:
+	int degree[SIZE];
+	queue<int> queue;
+	vector<int> graph[SIZE];
+public:
+
+};
 
 int main()
 {
 
-#pragma region 넓이 우선 탐색
+#pragma region 위상 정렬
 
-	// 
+	// 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+	// 모든 정점을 차례대로 진행하는 알고리즘.
+
+	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없음.
+
+	// DAG (Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프.
+
+	// 시작 복잡도 : O (V + E)
+
+	// 위상 정렬 방법.
+
+	// 1. 진입 차수가 0인 정점을 Queue에 삽입.
+
+	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거함.
+
+	// 3. 간선 제거 이후에 진입 차수가 0인 된 정점을 Queue에 삽입함.
+
+	// 4. Queue가 비어있을 때까지 2번 ~ 3번을 반복합니다.
 
 #pragma endregion
 
