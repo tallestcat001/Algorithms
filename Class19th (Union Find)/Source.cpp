8a8b4b652b04934826569dbb50eae57138fdb0d7
tallestcat001 <shdmlgus123_@naver.com>
2024-07-30 19:46:10 +0900

    (2024.7.30) 최소 신장 트리

---
+++ Class19th (Union Find)/Source.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+#define SIZE 7
+
+using namespace std;
+
+class Graph
+{
+private:
+	int parent[SIZE];
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			parent[i] = 0;
+		}
+	}
+
+	int Find(int x)
+	{
+		if (x == parent[x])
+		{
+			return x;
+		}
+		else
+		{
+			return parent[x] = Find(parent[x]);
+		}
+	}
+
+	void Union(int x, int y)
+	{
+		x = Find(x);
+		y = Find(y);
+
+		if (x < y)
+		{
+			parent[y] = x;
+		}
+		else
+		{
+			parent[x] = y;
+		}
+	}
+
+	bool Same(int x, int y)
+	{
+		return Find(x) == Find(y);
+	}
+};
+
+int main()
+{
+
+#pragma region 유니온 파인드
+
+	// 여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어 있는지 확인하는 알고리즘.
+
+	// 유니온 파인드의 시간 복잡도
+	// O(MlogN) : M은 연산의 개수, N은 노드의 개수
+	// M이 N²에 가까울 때는 O(N²lonN)이 됨.
+
+	Graph graph;
+
+	graph.Union(2, 3);
+	graph.Union(1, 3); // 1 2 1
+
+	cout << graph.Same(2, 3) << endl;
+	cout << graph.Same(1, 3) << endl;
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,33 +1,86 @@
 ﻿#include <iostream>
-
-#define SIZE 7
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 class Graph
 {
 private:
-	int parent[SIZE];
+	class Edge
+	{
+	private:
+		int x;
+		int y;
+		int distance;
+	public:
+		Edge(int x, int y, int distance)
+		{
+			this->x = x;
+			this->y = y;
+			this->distance = distance;
+		}
+
+		int& x()
+		{
+			return x;
+		}
+
+		int& y()
+		{
+			return y;
+		}
+
+		int& distance()
+		{
+			return distance;
+		}
+	};
+
+	vector<Edge> graph;
+
 public:
 	Graph()
 	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			parent[i] = 0;
-		}
+
+	}
+
+	void Insert(int x, int y, int distance)
+	{
+		graph.push_back(Edge(x, y, distance));
 	}
 };
 
 int main()
 {
 
-#pragma region 유니온 파인드
+#pragma region 신장 트리
 
-	// 여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어 있는지 확인하는 알고리즘.
+	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는 
+	// 부분 그래프로 그래프의 모든 정점을 최소 비용으로 연결하는 트리.
 
-	// 유니온 파인드의 시간 복잡도
-	// O(MlogN) : M은 연산의 개수, N은 노드의 개수
-	// M이 N²에 가까울 때는 O(N²lonN)이 됨.
+	// 그래프의 정점의 수가 n개 일때, 간선의 수는 n-1개.
+
+	// 최소 비용 신장 트리
+	// 그래프의 간선들의 가중치 합이 최소인 신장 트리.
+
+	Graph graph;
+
+	graph.Insert(1, 7, 10);
+	graph.Insert(1, 4, 28);
+	graph.Insert(1, 2, 66);
+	graph.Insert(1, 5, 19);
+
+	graph.Insert(2, 4, 25);
+	graph.Insert(2, 5, 60);
+
+	graph.Insert(3, 5, 21);
+	graph.Insert(3, 6, 35);
+
+	graph.Insert(4, 7, 15);
+
+	graph.Insert(5, 6, 40);
+
 
 #pragma endregion
 
