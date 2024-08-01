90ee88e22e0b19658ce2797c1a59848320d4d343
tallestcat001 <shdmlgus123_@naver.com>
2024-08-01 19:46:14 +0900

    (2024.8.1) 다이스트라 알고리즘

---
+++ Class21th (Prefix Sum)/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+
+#pragma region 누적 합
+
+	// 특정한 배열이 있을 때 배열까지의 합을 나타내는 것.
+
+	int one[SIZE] = { 1,2,3,4,5 };
+	int two[SIZE] = { 0,0,0,0,0 };
+
+	two[0] = one[0];
+
+	for (int i = 1; i < SIZE; i++)
+	{
+		two[i] = two[i - 1] + one[i];
+
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << two[i] << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,31 +1,68 @@
 ﻿#include <iostream>
 
-#define SIZE 5
+#define SIZE 6
+#define INF 10000000
 
 using namespace std;
+
+class Graph
+{
+private:
+	int weight[SIZE][SIZE] =
+	{
+		{0,2,5,3,INF,INF},
+		{2,0,7,INF,INF,9},
+		{5,7,0,1,5,2},
+		{3,INF,1,0,7,INF},
+		{INF,INF,2,7,0,2},
+		{INF,9,5,INF,2,0},
+	};
+
+	bool visited[SIZE];
+	int distance[SIZE];
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = 0;
+		}
+
+	}
+
+	int SmallNode()
+	{
+
+	}
+
+	void Dijkstra(int start)
+	{
+
+	}
+};
 
 int main()
 {
 
-#pragma region 누적 합
+#pragma region 다익스트라 알고리즘
 
-	// 특정한 배열이 있을 때 배열까지의 합을 나타내는 것.
+	// 시작점으로 부터 모든 노드까지의 최소 거리를 구해주는 알고리즘.
 
-	int one[SIZE] = { 1,2,3,4,5 };
-	int two[SIZE] = { 0,0,0,0,0 };
+	// 1. distance 배열에 weight[시작점 노드]의 값들로 초기화함.
 
-	two[0] = one[0];
+	// 2. 시작점을 방문처리함.
 
-	for (int i = 1; i < SIZE; i++)
-	{
-		two[i] = two[i - 1] + one[i];
-		
-	}
+	// 3. distance 배열에서 최소 비용 노드를 찾고 방문 처리함.
+	// 단, 이미 방문한 노드는 제외.
 
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << two[i] << " ";
-	}
+	// 4. 최소 비용 노드를 거쳐갈 지 고민해서 distance 배열을 갱신함.
+	// 단, 이미 방문한 노드는 제외.
+
+	// 5. 모든 노드를 방문할 때까지 3 ~  4번을 반복.
+
+	// 방문하지 않은 노드 중에서 가장 작은 distance를 가진 노드를 방문하고, 그 노드와 연결된 다른 노드까지의 거리를 계산함. 
+
+
 
 #pragma endregion
 
